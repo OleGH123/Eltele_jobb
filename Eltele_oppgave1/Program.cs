c8b245dfc977d868903a294f9a5f9dfaacc88932
Fredrik Stock <fredrik-stock@users.noreply.github.com>
2017-09-25 14:02:16 +0200

    review
    "negativ" review der kun det som kan/bør endres er nevnt.
    alt som ikke er kommentert på er med andre ord ok :)

--- Eltele_oppgave1/Program.cs
+++ Eltele_oppgave1/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+using System;
+
+// Unødvendige usings her, burde fjernes.
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,10 +13,13 @@ namespace Eltele_oppgave1
     {
         static void Main(string[] args)
         {
+            // pokemon-trycatch er ikke så veldig lurt. skal man håndtere errors bør det være eksplisitt.
             try
             {
                 //Fikser litt på titel
                 Console.Title = "Eltele Alternative 1";
+                
+                // vi trenger ikke dette tallet, vi skal gå gjennom alle tall 1..100
                 var number_read = Int32.Parse(Console.ReadLine()); //Converter string til int.
 
                 Check_div(number_read); //starter delt på funksjon
@@ -26,6 +31,7 @@ namespace Eltele_oppgave1
             
         }
 
+        // Unødvendig, kravspec har ikke bedt om looping :)
         public static void Fnc_keepgoing()
         {
             Console.WriteLine();
@@ -51,28 +57,44 @@ namespace Eltele_oppgave1
 
         public static void Check_div(int number)
         {
+            // Kommentar på avveie
             //Vi bruker try så om noe er feil så krajser ikke programet
             Console.Clear();
 
 
             //Var for og telle hvormange av de vi har
+            
+            // kan bruke implisitt typing i stedet for eksplisitt, dvs "var Amount_kakeboks = 0;".
+            // Det forstås fra sammenhengen at det er en int
+            
+            // navngivning: viktig å være konsekvent med navn, og følge standarder. gjelder for variabler og metoder.
+            // lokale variabler som disse skal for eksempel være camelCase, og underscores hører stortsett ikke hjemme noen sted.
+            // eks: var amountKakeBoks = 0;
             int Amount_kakeboks = 0;
             int Amount_Kake = 0;
             int Amount_Boks = 0;
 
+            // trycatch er unødvendig.
             try
             {
 
                 //Start for loop
-                for (int i = 0; i < 100; i++)
+                for (int i = 0; i < 100; i++) // vi skal ha 1..100, så denne bør være (var i=1;i<=100;i++)
                 {
+                    // Denne if() kan fjernes, gjør ikke noe vettugt
                     if (i % number == 0) //Starter deltpå
                     {
                         if (i % 3 == 0 && i % 5 == 0) //Sjekke kakeboks 
                         {
                             Console.Write("Kakeboks,");
-                            Amount_kakeboks = Amount_kakeboks + 1;
+                            Amount_kakeboks = Amount_kakeboks + 1; // Amount_kakeboks++; er ryddigere
                         } else
+                            
+                            // Her burde det unngås nesting for å holde koden lesbar.
+                            //   if {} elseif {} elseif {} else {}
+                            // er mer lesbart enn 
+                            //   if {} else { if {} else {} }
+                            
                         {
                             if (i % 3 == 0) //Sjekke kake
                             {
