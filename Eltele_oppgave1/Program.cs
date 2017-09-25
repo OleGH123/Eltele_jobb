cdc5f1cb9f0cd3b98bf005c0f33d0dc2aad5b65d
Ole <gunnar_holten@hotmail.com>
2017-09-25 13:37:39 +0200

    0.2

--- Eltele_oppgave1/Program.cs
+++ Eltele_oppgave1/Program.cs
@@ -13,13 +13,11 @@ namespace Eltele_oppgave1
         {
             try
             {
-                //Fikser litt på titel å må bruke program div = new... pga vi er i Main.
+                //Fikser litt på titel
                 Console.Title = "Eltele Alternative 1";
-                Program div = new Program();
                 var number_read = Int32.Parse(Console.ReadLine()); //Converter string til int.
 
-                div.check_div(number_read); //starter delt på funksjon
-                Console.ReadLine();
+                Check_div(number_read); //starter delt på funksjon
             } catch (Exception errors) //Ser etter feil om noe er feil under oppstart
             {
                 Console.WriteLine("ERROR: {0}", errors);
@@ -28,63 +26,88 @@ namespace Eltele_oppgave1
             
         }
 
-        public static class Globals
+        public static void Fnc_keepgoing()
         {
-            public static int global_candivon = 1; //global variables
-        }
-
-        public void check_div(int number)
-        { 
-            //Vi bruker try så om noe er feil så krajser ikke programet
-            try
+            Console.WriteLine();
+            Console.WriteLine("Type 'yes' if you wanna keep going or type 'exit' to exit.");
+            Console.WriteLine();
+            var str_keepgoing = Console.ReadLine(); //Henter info fra linja
+            
+            if (str_keepgoing == "yes") //Vis han skreiv yes så starter vi på nytt men først trenger vi et tall vi skal bruke
             {
-                int number_candivon = Globals.global_candivon; //Henter global variables og bruker valuen til en local var
+                Console.WriteLine("Type a number you wanna devide on.");
+                var new_number = Int32.Parse(Console.ReadLine()); //henter tall, convert fra str til int
 
-                if (number_candivon % number == 0) //Starter deltpå
+                if (new_number < 1)
                 {
-                    if (number_candivon == 3)
-                    {
-                        Console.Write("Kake");
-                    }
-                    else
-                    {
-                        if (number_candivon == 5)
-                        {
-                            Console.Write("Boks");
-                        }
-                    }
-
-                }
-
-                Globals.global_candivon = Globals.global_candivon + 1; //Siden vi skal telle fra 1-100 så bruker jeg dette 
-
-                if (Globals.global_candivon > 100) //Når den blir 100 så stopper vi
-                {
-                    Console.WriteLine();
-                    Console.WriteLine("{0}", number);
-                    Globals.global_candivon = 1;
-                    var new_number = Int32.Parse(Console.ReadLine());
-
-                    if (new_number < 1)
-                    {
-                        Console.WriteLine("Can't be a number less then 1.");
-                    }
-                    else
-                    {
-                        check_div(new_number); //Om vi er høyre en 0 så jobber vi videre med nytt tall
-                    }
-
+                    Console.WriteLine("Can't be a number less then 1.");
                 }
                 else
                 {
-
-                    check_div(number); //Om vi ikke er 100 så teller vi videre.
+                    Check_div(new_number);
                 }
+            }
+        }
+
+        public static void Check_div(int number)
+        {
+            //Vi bruker try så om noe er feil så krajser ikke programet
+            Console.Clear();
+
+
+            //Var for og telle hvormange av de vi har
+            int Amount_kakeboks = 0;
+            int Amount_Kake = 0;
+            int Amount_Boks = 0;
+
+            try
+            {
+
+                //Start for loop
+                for (int i = 0; i < 100; i++)
+                {
+                    if (i % number == 0) //Starter deltpå
+                    {
+                        if (i % 3 == 0 && i % 5 == 0) //Sjekke kakeboks 
+                        {
+                            Console.Write("Kakeboks,");
+                            Amount_kakeboks = Amount_kakeboks + 1;
+                        } else
+                        {
+                            if (i % 3 == 0) //Sjekke kake
+                            {
+                                Console.Write("Kake,");
+                                Amount_Kake = Amount_Kake + 1;
+                            } else
+                            {
+                                if (i % 5 == 0) //Sjekke boks
+                                {
+                                    Console.Write("Boks,");
+                                    Amount_Boks = Amount_Boks + 1;
+                                } else
+                                {
+                                    Console.Write("{0},", i); //Print tallet om ingen av de gikk.
+                                }
+                            }
+                        }                        
+                    }                  
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+
+                //Print ut hvormange vi har av ver
+                Console.WriteLine("Amount Kakeboks: {0}", Amount_kakeboks);
+                Console.WriteLine("Amount Kake: {0}", Amount_Kake);
+                Console.WriteLine("Amount Boks: {0}", Amount_Boks);
+
+                Console.WriteLine();
+                Fnc_keepgoing(); //Spør om brukeren har løst og prøve igjen
             } catch (Exception errors) //Feil meldinger
             {
                 Console.WriteLine("ERROR: {0}", errors);
                 Console.ReadLine();
-            }          
+            }
+            Console.ReadKey();
         }
     }
 }
